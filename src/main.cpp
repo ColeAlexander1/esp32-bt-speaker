314fa807d164a3018dd7cc8c90f6e3ef1078bce7
Colton Restell <coltonrestell@Coltons-Laptop.local>
2026-06-04 21:10:53 -0700

    .h

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "oled_utility.h"
 
 
 #define SCREEN_WIDTH 128
@@ -8,56 +9,24 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-// Fake UI data (you will replace later with Bluetooth data)
-int volume = 50;
-String songName = "YOOOOO";
-String timeRemaining = "02:35";
-
 void setup() {
-  Serial.begin(115200);
-
-  Wire.begin(21, 22);
-
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println("OLED init failed");
+    Serial.begin(115200);
+     // Start OLED - Check OLED Is Wired Correctly
+if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+{
+    Serial.println("OLED not found");
     while (true);
-  }
+}
+    display.clearDisplay(); 
 
-  display.clearDisplay();
+    DrawCenteredText(display, "Hello, World!", 1, 30);
+    DrawVolumeBar(display, 75);
+    
+    display.display();
+
 }
 
 void loop() {
-  display.clearDisplay();
 
-  // ===== Volume (top bar) =====
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-
-  display.print("Vol: ");
-  display.print(volume);
-  display.print("%");
-
-  // ===== Song name (middle) =====
-  display.setCursor(0, 20);
-  display.setTextSize(2);
-  display.print(songName);
-
-  // ===== Time remaining (bottom) =====
-  display.setTextSize(1);
-  display.setCursor(0, 50);
-  display.print("Left: ");
-  display.print(timeRemaining);
-
-  display.display();
-
-  delay(100);
-
-  volume++;
-
-  if (volume >= 100){
-
-    volume = 0;
-  }
 
 }
--- src/oled_utility.h
+++ src/oled_utility.h
@@ -3,14 +3,25 @@
 
 #include <Adafruit_SSD1306.h>
 
+
 void DrawCenteredText(Adafruit_SSD1306 &display, String text, int textsize, int y) 
 {
+    //Writes Text Centered On The OLED Display
     int x = (128-(text.length() * 6 * textsize)) / 2;
 
     display.setTextSize(textsize);
     display.setTextColor(WHITE);
     display.setCursor(x, y);
     display.println(text);
+}
+
+void DrawVolumeBar(Adafruit_SSD1306 &display, int volume) 
+{
+    // Draws a volume bar on the OLED display based on the volume level (0-100)
+    int barWidth = (volume * 128) / 100; 
+    
+    display.fillRect(0, 50, barWidth, 5, WHITE); 
+    display.drawRect(0, 50, 128, 5, WHITE); 
 }
 
 #endif // OLED_UTILITYH
